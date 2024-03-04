7173bdf4846fdc346935bd4ec2e69647d6c97a0b
panickus <felipedg@outlook.es>
2024-03-04 10:17:01 +0100

    Completado el menú hamburguesa y el cambio de tono B/N

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,23 +2,27 @@ import styles from './navbar.module.css'
 import Link from 'next/link'
 import AuthLinks from '../authLinks/AuthLinks'
 import ThemeToggle from '../themeToggle/ThemeToggle'
+import { FaFacebookF, FaInstagram, FaTiktok, FaYoutube, FaTwitter, FaDiscord } from 'react-icons/fa';
+
+
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.social}></div>
-      <img src="/public/facebook.png" alt="facebook" width={24} height={24}/>
-      <img src="/public/instagram.png" alt="instagram" width={24} height={24}/>
-      <img src="/public/tiktok.png" alt="tiktok" width={24} height={24}/>
-      <img src="/public/youtube.png" alt="youtube" width={24} height={24}/>
-      <img src="/public/twitter.png" alt="twitter" width={24} height={24}/>
-      <img src="/public/discord.png" alt="discord" width={24} height={24}/>
-      <div className={styles.logo}>Los Parera's Blog</div>
+      <div className={styles.social}>
+      <FaFacebookF size="24px" />
+      <FaInstagram size="24px" />
+      <FaTiktok size="24px" />
+      <FaYoutube size="24px" />
+      <FaTwitter size="24px" />
+      <FaDiscord size="24px" />
+      </div>
+      <div className={styles.logo}>The BlogOff</div>
       <div className={styles.links}>
         <ThemeToggle />
-        <Link href="/">Homepage</Link>
-        <Link href="/">Contact</Link>
-        <Link href="/">About</Link>
+        <Link href="/" className={styles.link}>Homepage</Link>
+        <Link href="/" className={styles.link}>Contact</Link>
+        <Link href="/" className={styles.link}>About</Link>
         <AuthLinks />
       </div>
     </div>
