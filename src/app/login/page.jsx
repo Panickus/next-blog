dbf43781f50bdfee7552f7325b9241d93da39870
panickus <felipedg@outlook.es>
2024-03-06 21:03:31 +0100

    Complete with errors Next.js How to Sign in with Google 02:33:48 Next.js MongoDB Prisma Tutorial (App Router)

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,11 +1,22 @@
 "use client";
+import { signIn, useSession } from "next-auth/react";
 import styles from "./loginPage.module.css";
-
+import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
+  const { data, status } = useSession();
 
+  const router = useRouter();
+  
+  if (status === "loading") {
+    return <div className={styles.loading}>Loading...</div>;
+  }
+  
+  if (status === "authenticated") {
+    router.push("/")
+  }
 
-    return (
+  return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <div className={styles.buttonSocial} onClick={() => signIn("google")}>
@@ -18,4 +29,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage
+export default LoginPage;
